2dfb8b9dc69a02eed311dac3c4a4eef536651859
arellacoder2013 <arellamwinga@gmail.com>
2024-11-22 18:59:20 +0300

    Update Parrot bird.py

--- Parrot bird.py
+++ Parrot bird.py
@@ -5,6 +5,10 @@ class Parrot:
     def __init__(self,name,age):
         self.name=name
         self.age=age
+    def sing(self,song):
+        return "{} sings {}".format(self.name,song)
+    def dance(self):
+        return "{} is now dancing".format(self.name)
 #instantiate the Parrot class
 blu=Parrot("Blu",10)
 woo=Parrot("Woo",15)
@@ -14,4 +18,7 @@ print("Woo is a {}".format(woo.species))
 #access the instant attributes
 print("{} is {} years old".format(blu.name,blu.age))
 print("{} is {} years old".format(woo.name,woo.age))
+print(blu.sing("Happy"))
+print(blu.dance())
+
                    
