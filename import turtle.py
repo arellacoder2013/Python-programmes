3cca1ca4e57b5ab17f059716b9f793833b876ec1
arellacoder2013 <arellamwinga@gmail.com>
2024-11-29 16:51:30 +0300

    ok

---
+++ import turtle.py
@@ -0,0 +1,28 @@
+import turtle
+turtle.Screen().bgcolor("green")
+board=turtle.Turtle()
+# Triangle
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+turtle.done()
+square=turtle.Turtle()
+# Square
+square.forward(100)
+
+board.left(120)
+square.forward(100)
+
+square.left(120)
+square.forward(100)
+
+square.left(120)
+square.forward(100)
+turtle.mainloop()
---
+++ natural number.py
@@ -0,0 +1,7 @@
+number = int(input("Enter Number to Check"))
+print("Number to be checked:",number)
+if number%0:
+    print("This is a natural number")
+else:
+    print("This is not a natural number")
+
