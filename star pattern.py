a7c6cb3c0df57951e6f959221dfe5fc2f71044e0
arellacoder2013 <arellamwinga@gmail.com>
2024-12-05 19:04:51 +0300

    ok

---
+++ star pattern.py
@@ -0,0 +1,9 @@
+for i in range(1,6):
+    for j in range(i):
+        print("*",end="")
+    print('\n')
+print("Inverted Star Pattren \n")
+for i in range(6,1,-1):
+    for j in range(i,1,-1):
+        print("abc",end="")
+    print('\n')
