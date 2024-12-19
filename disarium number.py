fdecb841c69dc5c509f17e67281059a335bae58f
arellacoder2013 <arellamwinga@gmail.com>
2024-12-19 10:02:25 +0300

    Update disarium number.py

--- disarium number.py
+++ disarium number.py
@@ -1,6 +1,19 @@
 import math
 def check(n):
-    count_digits=len(str(n))
-    sum=0
-    x=n
-    while(x!=0):
+    count_digits =len(str(n))
+    sum = 0
+    x = n
+    while (x!=0):
+        r = x % 10
+        sum = (int)  (sum+math.pow(r,count_digits))
+        count_digits=count_digits -1
+        x = x//10
+    if sum==n:
+        return 1
+    else:
+        return 0
+n=65
+if (check(n) == 1):
+    print("Disarium number")
+else :
+    print("Not a Disarium")
