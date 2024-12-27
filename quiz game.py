316320cbc71c467603dee31930b6a663d1c9f167
arellacoder2013 <arellamwinga@gmail.com>
2024-12-27 19:47:15 +0300

    Create quiz game.py

---
+++ quiz game.py
@@ -0,0 +1,17 @@
+print("Welcome to math quiz")
+ans1=input("What is 1000x50 a or b Press enter to enter your choice")
+print("a:5000 ,b:50000")
+ch=input()
+if ch =='b'or ch=='B':
+    print("correct")
+else:
+    print ("incorrect")
+print("Time for another question")
+ans2=input("What is 448x32 a or b Press enter to enter your choice")
+print("a:14,336 , b:15,336")
+ch=input()
+if ch =='a'or ch=='A':
+    print("correct")
+else:
+    print ("incorrect")
+    
