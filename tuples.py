49e73debed60740afc32d3db462ae2b0cf1c45c7
arellacoder2013 <arellamwinga@gmail.com>
2025-01-31 18:58:32 +0300

    9

---
+++ tuples.py
@@ -0,0 +1,13 @@
+tuplex =("tuple",False,3.2,1)
+print(tuplex)
+tuplex = (4,6,2,8,3,1)
+print(tuplex)
+#you can not add new elements they are immutable
+#add an element with'+'operator
+tuplex=tuplex+(9,)
+print(tuplex)
+tuple1 = (50,10,60,70,50)
+print(tuple1.count(50))
+tuplex = (2,4,3,5,4,6,7,6,1)
+_slice = tuplex[3:5]
+print(_slice)
