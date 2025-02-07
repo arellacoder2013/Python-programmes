5393f242fa51dba495c2be9eb529a5c4cd298877
arellacoder2013 <arellamwinga@gmail.com>
2025-02-07 18:51:32 +0300

    hhjk

---
+++ intersection of sets.py
@@ -0,0 +1,8 @@
+setx = {"green","blue"}
+sety = {"blue","yellow"}
+print("Oringinal set elements:")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets:")
+setz = setx.intersection(sety)
+print(setz)
