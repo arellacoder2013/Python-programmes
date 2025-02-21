1f806d7ec39f3aca925901e5183cbbea51fa5f3d
arellacoder2013 <arellamwinga@gmail.com>
2025-02-21 19:46:10 +0300

    p

---
+++ list comprehension practice.py
@@ -0,0 +1,6 @@
+numbers = [1,2,3,4,5,6,7,8,9,10]
+even = [x for x in numbers if x%2==0]
+odd = [x for x in numbers if x%2==1 ]
+print("List of even numbers from original:",even)
+print("List of odd numbers from original:",odd)
+
