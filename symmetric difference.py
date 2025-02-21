1a0cad4ee243cab6123e5d28066026e8712b1d0d
arellacoder2013 <arellamwinga@gmail.com>
2025-02-21 17:12:08 +0300

    Create symmetric difference.py

---
+++ symmetric difference.py
@@ -0,0 +1,8 @@
+setx = {"green","pink"}
+sety = {"pink","blue"}
+print("Oringinal set elements:")
+print(setx)
+print(sety)
+print("\nSymmetric difference of  the two said sets:")
+setz = setx.symmetric_difference(sety)
+print(setz)
