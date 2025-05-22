9bfa8d3f1afd4e2f1e1d85eb1cf375c81433cc13
arellacoder2013 <arellamwinga@gmail.com>
2025-05-22 18:58:51 +0300

    Add files via upload

---
+++ asymptotic  notation iterations.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    iteration=0
+    print("The  number entered by user is",n)
+    iteration+=1
+    print("Total iterations done by the code is",iteration,"\n")
+printnumber(90)
+printnumber(100)
+print("\n With any 'n' the time taken by our code won't")
