2fc9dd31ea762e66e35fa3f53a7f357828051549
arellacoder2013 <arellamwinga@gmail.com>
2025-07-17 19:44:23 +0300

    Add files via upload

--- power of 8.py
+++ power of 8.py
@@ -1,13 +1,6 @@
 def is_power_of_8(n):
     if not (n > 0 and (n & (n - 1)) == 0):
         return False
-    # Check if the single set bit is at a position that is a multiple of 3
-    # This mask checks for bits at positions 0, 3, 6, 9, ...
-    # Any power of 8 will have its single set bit at one of these positions.
-    # The mask ensures that if the number has a single set bit,
-    # it must align with one of these "power of 8" positions.
-    # Note: This mask is for 64-bit integers.
-    # For smaller integers, a shorter mask can be used, e.g., 0xB6DB6DB6 for 32-bit.
     mask = 0x249249249249249  
     return (n & mask) == n
 
